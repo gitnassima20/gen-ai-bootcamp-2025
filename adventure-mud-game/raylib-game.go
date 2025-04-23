7c5fb9c168d89bd00734f90df36cf55af3100dde
gitnassima20 <nassimabaroudi8@gmail.com>
2025-04-23 08:31:26 +0100

    #17 Added player Animation

--- adventure-mud-game/raylib-game.go
+++ adventure-mud-game/raylib-game.go
@@ -14,8 +14,15 @@ var (
 	bkgColor     = rl.NewColor(147, 211, 196, 255)
 	grasseSprite rl.Texture2D
 	playerSprite rl.Texture2D
-	playerSrc    rl.Rectangle
-	playerDest   rl.Rectangle
+
+	playerSrc                                     rl.Rectangle
+	playerDest                                    rl.Rectangle
+	playerMoving                                  bool
+	playerDir                                     int
+	playerUp, playerDown, playerRight, playerLeft bool
+	playerFrame                                   int
+
+	frameCount int
 
 	playerSpeed float32 = 3
 
@@ -32,19 +39,31 @@ func drawScene() {
 
 func input() {
 	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
-		playerDest.Y += playerSpeed
+		playerDest.Y -= playerSpeed
+		playerMoving = true
+		playerDir = 1
+		playerUp = true
 	}
 
 	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
-		playerDest.Y -= playerSpeed
+		playerDest.Y += playerSpeed
+		playerMoving = true
+		playerDir = 0
+		playerDown = true
 	}
 
 	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
 		playerDest.X -= playerSpeed
+		playerMoving = true
+		playerDir = 2
+		playerLeft = true
 	}
 
 	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
 		playerDest.X += playerSpeed
+		playerMoving = true
+		playerDir = 3
+		playerRight = true
 	}
 
 	if rl.IsKeyPressed(rl.KeyQ) {
@@ -55,6 +74,32 @@ func input() {
 func update() {
 	running = !rl.WindowShouldClose()
 
+	if playerMoving {
+		if playerUp {
+			playerDest.Y -= playerSpeed
+		}
+		if playerDown {
+			playerDest.Y += playerSpeed
+		}
+		if playerLeft {
+			playerDest.X -= playerSpeed
+		}
+		if playerRight {
+			playerDest.X += playerSpeed
+		}
+		if frameCount%8 == 1 {
+			playerFrame++
+		}
+	}
+
+	frameCount++
+	if playerFrame > 3 {
+		playerFrame = 0
+	}
+
+	playerSrc.X = playerSrc.Width * float32(playerFrame)
+	playerSrc.Y = playerSrc.Height * float32(playerDir)
+
 	rl.UpdateMusicStream(bkgSound)
 
 	if bkgSoundPaused {
@@ -64,6 +109,9 @@ func update() {
 	}
 
 	cam.Target = rl.NewVector2(float32(playerDest.X-playerDest.Width/2), float32(playerDest.Y-playerDest.Height/2))
+
+	playerMoving = false
+	playerUp, playerDown, playerRight, playerLeft = false, false, false, false
 }
 
 func render() {
